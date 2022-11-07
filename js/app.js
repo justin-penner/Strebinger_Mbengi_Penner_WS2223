50a819d823749d0693c1931883d134038f6467b9
Paul <>
2022-11-07 16:56:28 +0100

    Started with proof of concept

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const {covidHistory} = require("./covidApi.js")
 const {hotelForCity, getCities} = require("./hotelApi.js");
+const {getPointsOfInterest} = require("./sightseeingApi.js")
 
 const user = require("../controllers/userController.js")
 //router for User
@@ -16,5 +17,12 @@ app.get('/covid', async function (req, res) {
 app.get('/hotels', async function(req, res) {
    getCities(req, res);
 })
+
+//router for SightseeingApi
+app.get('/poi', async function(req, res) {
+    res.send(await getPointsOfInterest(req, res));
+ })
+
+ // 48.864716, 2.349014 for Paris
 
 app.listen(3000);
--- js/sightseeingApi.js
+++ js/sightseeingApi.js
@@ -1,27 +1,39 @@
 const fetch = require("node-fetch");
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '9e901e3198msh328f043ebfacb97p100ef1jsnc64a274704d8',
-		'X-RapidAPI-Host': 'opentripmap-places-v1.p.rapidapi.com'
-	}
-};
+async function getPointsOfInterest(req, res) {
 
-const lon = 2.3522219;
-const lat = 48.856614;
+	let pointsOfInterest = Array();
 
-fetch('https://opentripmap-places-v1.p.rapidapi.com/en/places/radius?radius=100&lon=' + lon + '&lat=' + lat + '', options)
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '9e901e3198msh328f043ebfacb97p100ef1jsnc64a274704d8',
+			'X-RapidAPI-Host': 'opentripmap-places-v1.p.rapidapi.com'
+		}
+	};
+	
+	await fetch('https://opentripmap-places-v1.p.rapidapi.com/en/places/radius?radius=100&lon=' + req.query.lon + '&lat=' + req.query.lat, options)
 	.then(response => response.json())
 	.then(response => {
+	
+		const data = response.features;
 
+		for(let index = 0; index < data.length; index++) {
 
-        const data = response.features;
+				let name = data[index].properties.name;
+				let coordinates = data[index].geometry.coordinates;
+				let information = "https://www.wikidata.org/wiki/" + data[index].properties.wikidata;
+				let distance = data[index].properties.dist.toFixed(2) + "km";
 
-        data.length = 10;
+				pointsOfInterest.push({name, coordinates, distance, information});
 
-        console.log(data);
-
-
-    })
+		}		
+	
+	})
 	.catch(err => console.error(err));
+
+	return pointsOfInterest;
+
+}
+
+module.exports = {getPointsOfInterest};
