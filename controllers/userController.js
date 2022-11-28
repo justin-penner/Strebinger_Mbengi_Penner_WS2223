814092b4aebb1af8a55aeefacd6e4d0310c90cf4
justin-penner <justin.penner@inspired.consulting>
2022-11-28 11:53:34 +0100

    get json or html

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,20 +14,42 @@ exports.index = async function(req, res) {
 }
 
 exports.register = async function(req, res) {
-    res.status(200).sendFile('register.html', { root: path.join(__dirname, '../views') }); 
+    if(req.headers.accept != "application/json") {
+        res.status(200).sendFile('register.html', { root: path.join(__dirname, '../views') }); 
+    } else {
+        const json = {
+            "password":null,
+	        "name":null,
+	        "email":null
+        }
+        res.status(200).send(json);
+    }
 }
 
 exports.loginPage = async function(req, res) {
-    if(User.name == null && User.email == null && User.apikey == null) {console.log("Logged out!"); res.status(400).sendFile('login.html', { root: path.join(__dirname, '../views') })} else {res.status(200).redirect("/user")}
+    if(req.headers.accept != "application/json") {
+        if(User.name == null && User.email == null && User.apikey == null) {console.log("Logged out!"); res.status(400).sendFile('login.html', { root: path.join(__dirname, '../views') })} else {res.status(200).redirect("/user")}
+    } else {
+        const json = {
+            "password":null,
+	        "name":null,
+	        "email":null
+        }
+        res.status(200).send(json);
+    }
 }
 
 exports.info = async function(req, res) {
-    if(User.email != null && User.name != null && User.apikey != null) {
-        res.send("<p>Hello, " + User.name + "</p>" +
-        "<p>E-mail: " + User.email + "</p>" +
-        "<p>Api-Key: " + User.apikey + "</p>" + 
-        "<a href='/logout'>Logout</a>")
-    } else {console.log("Logged out!");res.status(400).sendFile('login.html', { root: path.join(__dirname, '../views') });}
+    if(req.headers.accept != "application/json") {
+        if(User.email != null && User.name != null && User.apikey != null) {
+            res.send("<p>Hello, " + User.name + "</p>" +
+            "<p>E-mail: " + User.email + "</p>" +
+            "<p>Api-Key: " + User.apikey + "</p>" + 
+            "<a href='/logout'>Logout</a>")
+        } else {console.log("Logged out!");res.status(400).sendFile('login.html', { root: path.join(__dirname, '../views') });}
+    } else {
+        res.status(200).send(User);
+    }
 }
 
 exports.login = async function(req, res) {
