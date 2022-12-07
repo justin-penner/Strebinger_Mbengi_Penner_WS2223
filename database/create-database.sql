6f8dc7f54697346e8be68d054889eb533f73480f
justin-penner <justin.penner@inspired.consulting>
2022-12-07 12:56:07 +0100

    Database drop Table on build

--- database/create-database.sql
+++ database/create-database.sql
@@ -1,5 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS users (id SERIAL primary key not null, name varchar(250) not null, email varchar(250) not null unique, password varchar(250), apikey uuid DEFAULT uuid_generate_v4());
-
+DROP IF EXISTS TABLE users;
+CREATE TABLE users (id SERIAL primary key not null, name varchar(250) not null, email varchar(250) not null unique, password varchar(250), apikey uuid DEFAULT uuid_generate_v4());
 INSERT INTO users (name, email, password) VALUES ('admin', 'admin@traveldb.mail', 'secret');
